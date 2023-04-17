659f133eec4dbccda5b0a4033db0b9be87afb3a1
Andi Gjetani <andigjetani@gmail.com>
2023-04-17 17:20:28 +0200

    Add project files.

---
+++ ManagerLogin/CookiesTest_V2.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Newtonsoft.Json;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class CookieTest_V2
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [Test]
+        public void firstLogin()
+        {
+            // Navigate to the login page and login
+            driver.Navigate().GoToUrl("https://qawa2.telecomsoftware.com/");
+            driver.FindElement(By.CssSelector("input[name='username']")).SendKeys("andi");
+            driver.FindElement(By.CssSelector("button[role='button']")).Click();
+            IWebElement logout = driver.FindElement(By.CssSelector("[class] [class] [showon='none']:nth-of-type(3)"));
+            Assert.IsTrue(logout.Displayed);
+            CookieHelper_V2.SaveCookieToFile(driver, "Qawa2Cookies1.json");
+        }
+
+        [Test]
+        public void secondLogin()
+        {
+            // Navigate to the login page
+            driver.Navigate().GoToUrl("https://qawa2.telecomsoftware.com/");
+
+            // Read cookies from the first test
+            CookieHelper_V2.LoadCookieFromFile(driver, "Qawa2Cookies1.json");
+
+            // Refresh the page to apply cookies
+            driver.Navigate().Refresh();
+
+            // Check if login was successful
+            IWebElement logout = driver.FindElement(By.CssSelector("[class] [class] [showon='none']:nth-of-type(3)"));
+            Assert.IsTrue(logout.Displayed);
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ ManagerLogin/Helpers/CookieHelper_V2.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using System.IO;
+using System.Text.Json;
+
+public static class CookieHelper_V2
+{
+
+    // Method to save a cookie as a file
+    public static void SaveCookieToFile(IWebDriver driver, string fileName)
+    {
+        // Get the current set of cookies from the driver
+        var cookies = driver.Manage().Cookies.AllCookies;
+
+        // Serialize the cookies to JSON format
+        var json = JsonSerializer.Serialize(cookies);
+
+        // Write the JSON data to a file
+        var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+        File.WriteAllText(filePath, json);
+    }
+
+    // Method to load a cookie from a file
+    public static void LoadCookieFromFile(IWebDriver driver, string fileName)
+    {
+        // Read the JSON data from the file
+        var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+        var json = File.ReadAllText(filePath);
+
+        // Deserialize the cookies from JSON format
+        var cookies = JsonSerializer.Deserialize<List<Cookie>>(json);
+
+        // Add the cookies to the driver
+        foreach (var cookie in cookies)
+        {
+            driver.Manage().Cookies.AddCookie(cookie);
+        }
+    }
+}
